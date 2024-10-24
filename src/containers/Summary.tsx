39614ad24208e746f6681deca774a336fa44ac9b
Kevin More <kore_2608@hotmail.com>
2024-10-24 05:07:42 -0500

    chore: pequeña corrección

--- src/containers/Summary.tsx
+++ src/containers/Summary.tsx
@@ -69,7 +69,7 @@ const SummaryContainer: React.FC = () => {
   }
   const openModal = () => {
     setIsModalOpen(true);
-  }; 
+  };
   return (
     <div>
       <Header openModal={openModal} />{" "}
@@ -80,20 +80,23 @@ const SummaryContainer: React.FC = () => {
         <div className="anchoroot">
           <button
             onClick={handleGoBack}
-            className="hidden sm:flex items-center text-blue-600 font-semibold mb-6"
+            className="flex items-center mobilebr text-blue-600 font-semibold mb-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-5 h-5 mr-2"
-            >
-              <path
-                fillRule="evenodd"
-                d="M15.53 4.47a.75.75 0 010 1.06L10.56 10.5h8.69a.75.75 0 010 1.5H10.56l4.97 4.97a.75.75 0 11-1.06 1.06l-6.25-6.25a.75.75 0 010-1.06l6.25-6.25a.75.75 0 011.06 0z"
-              />
-            </svg>
-            Volver
+            <span className="rounded-full border border-blue-600 p-1 flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-3 h-3"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15.53 4.47a.75.75 0 010 1.06L10.56 10.5h8.69a.75.75 0 010 1.5H10.56l4.97 4.97a.75.75 0 11-1.06 1.06l-6.25-6.25a.75.75 0 010-1.06l6.25-6.25a.75.75 0 011.06 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+            <span className="ml-2">Volver</span>
           </button>
 
           <h1 className="text-4xl font-bold text-left mb-4">
